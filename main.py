50214e2706fea338ee94bf7424b5bcdfa6c7d1bb
Victoriamdz <victoria.mendezlpz@gmail.com>
2025-03-20 14:35:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import data
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+import data
 
 
 # no modificar
@@ -37,15 +38,32 @@ def retrieve_phone_code(driver) -> str:
 class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
+    comfort_tariff = (By.CLASS_NAME, 'tariff-comfort')
+    phone_field = (By.ID, 'phone')
+    confirm_phone_button = (By.ID, 'confirm-phone')
+    card_number_field = (By.ID, 'card-number')
+    save_card_button = (By.ID, 'save-card')
+    message_field = (By.ID, 'driver-message')
+    blanket_checkbox = (By.ID, 'option-blanket')
+    tissues_checkbox = (By.ID, 'option-tissues')
+    ice_cream_checkbox = (By.ID, 'option-ice-cream')
+    order_button = (By.ID, 'order-ride')
+    driver_info = (By.CLASS_NAME, 'driver-info')
 
     def __init__(self, driver):
         self.driver = driver
 
     def set_from(self, from_address):
-        self.driver.find_element(*self.from_field).send_keys(from_address)
+        from_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.from_field)
+        )
+        from_element.send_keys(from_address)
 
     def set_to(self, to_address):
-        self.driver.find_element(*self.to_field).send_keys(to_address)
+        to_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.to_field)
+        )
+        to_element.send_keys(to_address)
 
     def get_from(self):
         return self.driver.find_element(*self.from_field).get_property('value')
@@ -53,6 +71,40 @@ class UrbanRoutesPage:
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
 
+    def select_comfort_tariff(self):
+        self.driver.find_element(*self.comfort_tariff).click()
+
+
+    def enter_phone(self, phone):
+        self.driver.find_element(*self.phone_field).send_keys(phone)
+        self.driver.find_element(*self.confirm_phone_button).click()
+
+    def enter_card_details(self, card_number, cvv):
+        self.driver.find_element(*self.card_number_field).send_keys(card_number)
+
+        # CVV
+        cvv_field = self.driver.find_element(By.ID, 'code')
+        cvv_field.send_keys(cvv)
+        cvv_field.send_keys(Keys.TAB)
+
+        self.driver.find_element(*self.save_card_button).click()
+
+    def enter_driver_message(self, message):
+        self.driver.find_element(*self.message_field).send_keys(message)
+
+    def select_extras(self):
+        self.driver.find_element(*self.blanket_checkbox).click()
+        self.driver.find_element(*self.tissues_checkbox).click()
+
+        ice_cream = self.driver.find_element(*self.ice_cream_checkbox)
+        ice_cream.click()
+        ice_cream.click()
+
+    def place_order(self):
+        self.driver.find_element(*self.order_button).click()
+
+    def wait_for_driver_info(self):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.driver_info))
 
 
 class TestUrbanRoutes:
@@ -62,20 +114,54 @@ class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
         # no lo modifiques, ya que necesitamos un registro adicional habilitado para recuperar el código de confirmación del teléfono
-        from selenium.webdriver import DesiredCapabilities
-        capabilities = DesiredCapabilities.CHROME
-        capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
-        cls.driver = webdriver.Chrome(desired_capabilities=capabilities)
+        options = webdriver.ChromeOptions()
+        options.set_capability("goog:loggingPrefs", {'performance': 'ALL'})
+        cls.driver = webdriver.Chrome(options=options)
 
     def test_set_route(self):
         self.driver.get(data.urban_routes_url)
         routes_page = UrbanRoutesPage(self.driver)
+
         address_from = data.address_from
         address_to = data.address_to
-        routes_page.set_route(address_from, address_to)
+
+        routes_page.set_from(address_from)
+        routes_page.set_to(address_to)
+
         assert routes_page.get_from() == address_from
         assert routes_page.get_to() == address_to
 
+    def test_complete_ride_order(self):
+        self.driver.get(data.urban_routes_url)
+        routes_page = UrbanRoutesPage(self.driver)
+
+        # Dirección
+        routes_page.set_from(data.address_from)
+        routes_page.set_to(data.address_to)
+
+        # Tarifa
+        routes_page.select_comfort_tariff()
+
+        # teléfono y confirmar
+        routes_page.enter_phone(data.phone_number)
+
+        # Agregar tarjeta
+        routes_page.enter_card_details(data.card_number, data.card_code)
+
+        # mensaje para el conductor
+        routes_page.enter_driver_message('Muéstrame el camino al museo')
+
+        # opciones extra
+        routes_page.select_extras()
+
+        # Realizar pedido
+        routes_page.place_order()
+
+        # Esperar información del conductor
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(UrbanRoutesPage.driver_info))
+
+        # Verificar que la información del conductor es visible
+        assert self.driver.find_element(*UrbanRoutesPage.driver_info).is_displayed()
 
     @classmethod
     def teardown_class(cls):
