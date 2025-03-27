3dbd3564207c777a9de5c4ff7dec747a9f394d3e
Victoriamdz <victoria.mendezlpz@gmail.com>
2025-03-26 18:20:38 -0600

    Update selectors.py

--- selectors.py
+++ selectors.py
@@ -1,16 +1,30 @@
 from selenium.webdriver.common.by import By
 
-# Selectores de Urban Routes
+# Campos de entrada
 from_field = (By.ID, 'from')
 to_field = (By.ID, 'to')
-comfort_tariff = (By.CLASS_NAME, 'tariff-comfort')
+
+# Tarifa
+comfort_tariff = (By.ID, 'tariff-comfort')
+
+# Número de teléfono
 phone_field = (By.ID, 'phone')
 confirm_phone_button = (By.ID, 'confirm-phone')
+
+# Método de pago
 card_number_field = (By.ID, 'card-number')
 save_card_button = (By.ID, 'save-card')
+
+# Mensaje para el conductor
 message_field = (By.ID, 'driver-message')
+
+# Checkboxes de opciones extra
 blanket_checkbox = (By.ID, 'option-blanket')
 tissues_checkbox = (By.ID, 'option-tissues')
 ice_cream_checkbox = (By.ID, 'option-ice-cream')
+
+# Botón para ordenar
 order_button = (By.ID, 'order-ride')
+
+# Información del conductor
 driver_info = (By.CLASS_NAME, 'driver-info')
