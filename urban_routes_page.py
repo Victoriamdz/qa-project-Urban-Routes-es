69a2782e81bfbd3dfdeddebf14a4ac1ecd1b3a7f
Victoriamdz <victoria.mendezlpz@gmail.com>
2025-03-26 18:42:19 -0600

    Update urban_routes_page.py

--- urban_routes_page.py
+++ urban_routes_page.py
@@ -1,4 +1,4 @@
-cfrom selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Keys
