5de0e033dc01e0541b481680af5036dee7419a93
Victoriamdz <victoria.mendezlpz@gmail.com>
2025-03-29 00:02:53 -0600

    Update test_urban_routes.py

--- test_urban_routes.py
+++ test_urban_routes.py
@@ -12,14 +12,14 @@ class TestUrbanRoutes:
 
     @classmethod
     def setup_class(cls):
-        """Configura el driver para Chrome y abre la URL de prueba."""
+       
         options = webdriver.ChromeOptions()
         options.set_capability("goog:loggingPrefs", {'performance': 'ALL'})
         cls.driver = webdriver.Chrome(options=options)
         cls.driver.get(data.urban_routes_url)
 
     def test_set_route(self):
-        """Verifica que las direcciones de origen y destino se establecen correctamente."""
+        """Verifica que las direcciones de origen y destino """
         routes_page = UrbanRoutesPage(self.driver)
 
         routes_page.set_from(data.address_from)
@@ -30,14 +30,14 @@ class TestUrbanRoutes:
         assert routes_page.get_to() == data.address_to
 
     def test_select_comfort_tariff(self):
-        """Verifica que se selecciona la tarifa Comfort."""
+        """Verifica la tarifa Comfort."""
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.select_comfort_tariff()
 
         assert routes_page.driver.find_element(*selectors.comfort_tariff).is_displayed()
 
     def test_set_phone_number(self):
-        """Prueba agregar número de teléfono y validar código de confirmación."""
+        """Prueba agregar número de teléfono y validar código."""
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.enter_phone(data.phone_number)
 
@@ -57,14 +57,14 @@ class TestUrbanRoutes:
         assert routes_page.driver.find_element(*selectors.save_card_button).is_displayed()
 
     def test_write_message(self):
-        """Verifica que se pueda enviar mensaje para el conductor."""
+        """Verifica mensaje para el conductor."""
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.enter_driver_message('Muéstrame el camino al museo')
 
         assert routes_page.driver.find_element(*selectors.message_field).is_displayed()
 
     def test_select_extras(self):
-        """Prueba seleccionar extras (frazada y helado)."""
+        """Prueba seleccionar extras"""
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.select_extras()
 
@@ -72,7 +72,7 @@ class TestUrbanRoutes:
         assert routes_page.driver.find_element(*selectors.ice_cream_checkbox).is_selected()
 
     def test_find_driver(self):
-        """Prueba la búsqueda de un conductor."""
+        """Prueba búsqueda de un conductor."""
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.place_order()
 
