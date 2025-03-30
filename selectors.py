0d69601957b8524720a7b60e1d6598dadf25e86c
Victoriamdz <victoria.mendezlpz@gmail.com>
2025-03-30 17:34:45 -0600

    Update selectors.py

--- selectors.py
+++ selectors.py
@@ -1,35 +1,35 @@
 from selenium.webdriver.common.by import By
 
 #  Campos de entrada
-from_field = (By.XPATH, '//*[@id="root"]/div/div[3]/div[2]/div[1]/div[2]/div[1]/label')
-to_field = (By.XPATH, '//*[@id="root"]/div/div[3]/div[2]/div[2]/div[2]/div[1]/label')
+from_field = (By.ID, 'from')
+to_field = (By.ID, 'to')
 
 #  Botón "Pedir un taxi"
 pedir_taxi = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[1]/div[3]/div[1]/button')
 
 
 #  Tarifa Comfort
-comfort_tariff = (By.XPATH, ‘//*[@id="root"]/div/div[3]/div[3]/div[2]/div[1]/div[5]/div[1]/img')
-
+comfort_tariff = comfort_tariff = (By.XPATH, "//div[@class='tcard-title' and text()='Comfort']")
 
 #  Número de teléfono
-phone_field = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[1]/div')
-confirm_phone_button = (By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[1]/form/div[2]/button')
+phone_field = (By.XPATH, "//div[text()='Número de teléfono']/following-sibling::input")
+confirm_phone_button = (By.XPATH, "//button[text()='Confirmar']")
 
 # Método de pago
-card_number_field = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[2]/div[1]')
-save_card_button = (By.XPATH, '//*[@id="root"]/div/div[2]/div[2]/div[2]/form/div[3]/button[1]')
+card_number_field = (By.XPATH, "//div[text()='Método de pago']/following-sibling::input")
+save_card_button = (By.XPATH, "//button[text()='Agregar']")
 
 # Mensaje para el conductor
-message_field = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[3]/div/label')
+message_field = (By.XPATH, "//label[text()='Mensaje para el conductor…']/following-sibling::textarea")
 
 #  Checkboxes de opciones extra
-blanket_checkbox = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[1]/div/div[2]/div/span')
+blanket_checkbox = (By.XPATH, "//span[@class='slider round']")
+
 
 ice_cream_checkbox = (By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]')
 
 #  Botón para ordenar viaje
-order_button = (By.XPATH, '//*[@id="root"]/div/div[3]/div[4]/button')
+order_button = (By.XPATH, "//span[text()='Introducir un número de teléfono y reservar']")
 
 #  Información del conductor
 driver_info = (By.XPATH, '//div[contains(@class, "driver-info")]')
